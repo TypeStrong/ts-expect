1027240a4c6ae2a33368254b95d2bdc9475db65f
Blake Embrey <hello@blakeembrey.com>
2021-02-08 14:49:35 -0800

    Update examples to typescript

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 /**
  * Checks that `Value` is assignable to `Target`. For example:
  *
- * ```js
- * TypeOf<number, 123> //=> true
- * TypeOf<123, number> //=> false
+ * ```ts
+ * expectType<TypeOf<number, 123>>(true);
+ * expectType<TypeOf<123, number>>(false);
  * ```
  */
 export type TypeOf<Target, Value> = Exclude<Value, Target> extends never
@@ -13,11 +13,11 @@ export type TypeOf<Target, Value> = Exclude<Value, Target> extends never
 /**
  * Checks that `Value` is equal to the same type as `Target`. For example:
  *
- * ```js
- * TypeEqual<number, number> //=> true
- * TypeEqual<number, 123> //=> false
- * TypeEqual<123, number> //=> false
- * TypeEqual<123, 123> //=> true
+ * ```ts
+ * expectType<TypeEqual<123, 123>>(true);
+ * expectType<TypeEqual<123, number>>(false);
+ * expectType<TypeEqual<number, 123>>(false);
+ * expectType<TypeEqual<number, number>>(true);
  * ```
  */
 export type TypeEqual<Target, Value> = Exclude<Target, Value> extends never
