9ce06f3980cc30fa19905f5171b088c70d5da711
Blake Embrey <hello@blakeembrey.com>
2021-02-09 17:47:04 -0800

    Improve type documentation (#12)

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,37 @@
 /**
- * Checks that `T` is of type `U`.
+ * Checks that `Value` is assignable to `Target`.
+ *
+ * ```ts
+ * expectType<TypeOf<number, 123>>(true);
+ * expectType<TypeOf<123, number>>(false);
+ * ```
  */
-export type TypeOf<T, U> = Exclude<U, T> extends never ? true : false;
+export type TypeOf<Target, Value> = Exclude<Value, Target> extends never
+  ? true
+  : false;
 
 /**
- * Checks that `T` is equal to `U`.
+ * Checks that `Value` is equal to the same type as `Target`.
+ *
+ * ```ts
+ * expectType<TypeEqual<123, 123>>(true);
+ * expectType<TypeEqual<123, number>>(false);
+ * expectType<TypeEqual<number, 123>>(false);
+ * expectType<TypeEqual<number, number>>(true);
+ * ```
  */
-export type TypeEqual<T, U> = Exclude<T, U> extends never ? Exclude<U, T> extends never ? true : false : false;
+export type TypeEqual<Target, Value> = Exclude<Target, Value> extends never
+  ? Exclude<Value, Target> extends never
+    ? true
+    : false
+  : false;
 
 /**
- * Assert the parameter is of a specific type.
+ * Asserts the `value` type is assignable to the generic `Type`.
+ *
+ * ```ts
+ * expectType<number>(123);
+ * expectType<boolean>(true);
+ * ```
  */
-export const expectType = <T>(_: T): void => undefined;
+export const expectType = <Type>(value: Type): void => void 0;
